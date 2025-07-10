5856a9bb4060c5b2f020f18e9576c72923a7a5da
AJYoung4 <AJYoung4@live.com>
2025-07-10 18:52:07 -0400

    basic static file server

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	mux := http.NewServeMux()
+
+	fs := http.FileServer(http.Dir("."))
+	mux.Handle("/", fs)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	fmt.Println("Server running on http://localhost:8080")
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println("Server error:", err)
+	}
 }
