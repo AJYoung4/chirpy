c7c79fa459c3ca67f491b5c1a34589a7f437e0fd
AJYoung4 <ajyoung4@live.com>
2025-07-16 12:05:39 -0400

    error fix from swapping pcs

--- main.go
+++ main.go
@@ -36,33 +36,3 @@ func main() {
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
 }
-
-import (
-	"fmt"
-	"net/http"
-
-	"github.com/AJYoung4/chirpy/api"
-)
-
-func main() {
-	mux := http.NewServeMux()
-	cfg := api.ApiConfig{}
-
-	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", cfg.MiddlewareMetricsInc(http.StripPrefix("/app/", fs)))
-
-	mux.HandleFunc("GET /api/healthz", api.ReadinessHandler)
-	mux.HandleFunc("GET /admin/metrics", cfg.MetricsHandler)
-	mux.HandleFunc("POST /admin/reset", cfg.ResetHandler)
-	mux.HandleFunc("POST /api/validate_chirp", api.ValidateHandler)
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
-	}
-
-	fmt.Println("Server running on http://localhost:8080")
-	if err := server.ListenAndServe(); err != nil {
-		fmt.Println("Server error:", err)
-	}
-}
